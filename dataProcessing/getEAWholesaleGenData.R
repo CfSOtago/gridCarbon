a312821d559655fc0a578cb710480e52a1dc505e
Ben Anderson <dataknut@icloud.com>
2019-05-27 14:03:47 +1200

    amended paths & re-run to update data

--- dataProcessing/getEAWholesaleGenData.R
+++ dataProcessing/getEAWholesaleGenData.R
@@ -8,37 +8,39 @@ rm(list=ls(all=TRUE)) # remove all objects from workspace
 
 
 # Load libraries ----
-library(girdCarbon) #load this first - you will need to download & install it locally from this repo
+library(gridCarbon) #load this first - you will need to download & install it locally from this repo
 
 # Packages needed in this .Rmd file ----
 reqLibs <- c("data.table", # data munching
-             "readr", # writing to files
              "curl",  #for data download
              "ggplot2", # for fancy graphs
+             "readr", # writing to files
              "skimr" # for skim
 )
 # load them
-loadLibraries(reqLibs)
+gridCarbon::loadLibraries(reqLibs)
 
 # Parameters ----
 
-local <- 0 # set to 1 for local file storage (see below)
+local <- 1 # set to 1 for local file storage (see below)
 refresh <- 0 # set to 1 to try to download all files even if we have them
 
 if(local){ # set data storage location
-  lDataLoc <- path.expand("~/Data/NZGreenGrid/safe/ea/")
+  iDataPath <- path.expand("~/Data/NZ_EA_EMI/gridGen/raw/")
+  oDataPath <- path.expand("~/Data/NZ_EA_EMI/gridGen/processed/")
 } else {
-  lDataLoc <- path.expand("/Volumes/hum-csafe/Research Projects/GREEN Grid/externalData/EA_Generation_Data/")
+  iDataPath <- path.expand("/Volumes/hum-csafe/Research Projects/GREEN Grid/externalData/EA_Generation_Data/raw/")
+  oDataPath <- path.expand("/Volumes/hum-csafe/Research Projects/GREEN Grid/externalData/EA_Generation_Data/processed/")
 }
 
 rDataLoc <- "https://www.emi.ea.govt.nz/Wholesale/Datasets/Generation/Generation_MD/"
-years <- seq(1997, 2018, 1) # change these to restrict or extend the file search
+years <- seq(1997, 2019, 1) # change these to restrict or extend the file search
 months <- seq(1,12,1) # change these to restrict or extend the file search
 
 # Local functions ----
 cleanEA <- function(df){
   # takes a df, cleans & returns a dt
-  dt <- data.table::as.data.table(df) # make dt
+  dt <- data.table::as.data.table(df) # make dt
   dt <- reshapeEAGenDT(dt) # make long
   dt <- setEAGenTimePeriod(dt) # set time periods to something intelligible as rTime
   dt <- dt[, rDate := as.Date(Trading_date)] # fix the dates so R knows what they are
@@ -63,7 +65,7 @@ getMeta <- function(dt){
 # Set start time ----
 startTime <- proc.time()
 
-filesToDateDT <- data.table::as.data.table(list.files(lDataLoc)) # get list of files already downloaded
+filesToDateDT <- data.table::as.data.table(list.files(iDataPath)) # get list of files already downloaded
 
 metaDT <- data.table::data.table() # stats collector
 
@@ -79,13 +81,14 @@ for(y in years){
     lfName <- paste0(y,"_", m,"_Generation_MD.csv") # for ease of future file filtering
     rfName <- paste0(y, m,"_Generation_MD.csv")
     print(paste0("Checking ", lfName))
-    test <- filesToDateDT[V1 %like% lfName] # should catch .csv.gz too
+    test <- filesToDateDT[V1 %like% lfName] #should catch .csv.gz too
     if(nrow(test) > 0 & refresh == 0){
       # Already got it & we don't want to refresh so skip
       print(paste0("Already have ", lfName, ", loading from local..."))
       # Load so we can update meta
-      df <- readr::read_csv(paste0(lDataLoc, lfName))
-      dt <- cleanEA(df) # clean up to a dt
+      df <- readr::read_csv(paste0(iDataPath, lfName)) # this loop wants the raw data
+      # should probably change this so it doesn't need to run over _all_ the files every time
+      dt <- cleanEA(df) # clean up to a dt - fixes dateTimes etc
       print(summary(dt))
       testDT <- getMeta(dt) # get metaData
       print(head(testDT))
@@ -96,21 +99,21 @@ for(y in years){
       # Get it
       rFile <- paste0(rDataLoc,rfName)
       print(paste0("We don't have or need to refresh ", lfName))
-      # use curl function to catch errors
+      # use curl function to catch errors
       print(paste0("Trying to download ", rFile))
       req <- curl::curl_fetch_disk(rFile, "temp.csv") # https://cran.r-project.org/web/packages/curl/vignettes/intro.html
       if(req$status_code != 404){ #https://cran.r-project.org/web/packages/curl/vignettes/intro.html#exception_handling
         df <- readr::read_csv(req$content)
         print("File downloaded successfully, saving it")
-        data.table::fwrite(df, paste0(lDataLoc, lfName))
-        dt <- cleanEA(df) # clean up to a dt
+        data.table::fwrite(df, paste0(iDataPath, lfName))
+        dt <- cleanEA(df) # clean up to a dt - this does all the processing
         testDT <- getMeta(dt) # get metaData
         testDT <- testDT[, source := rfName]
         metaDT <- rbind(metaDT, testDT)
         print("Converted to long form, saving it")
         lfName <- paste0(y,"_",m,"_Generation_MD_long.csv")
-        data.table::fwrite(dt, paste0(lDataLoc, lfName))
-        cmd <- paste0("gzip -f ", "'", path.expand(paste0(lDataLoc, lfName)), "'") # gzip it - use quotes in case of spaces in file name, expand path if needed
+        data.table::fwrite(dt, paste0(oDataPath, lfName))
+        cmd <- paste0("gzip -f ", "'", path.expand(paste0(oDataPath, lfName)), "'") # gzip it - use quotes in case of spaces in file name, expand path if needed
         try(system(cmd)) # in case it fails - if it does there will just be .csv files (not gzipped) - e.g. under windows
         print("Compressed it")
       } else {
@@ -121,11 +124,9 @@ for(y in years){
 }
 
 # load the meta data as a shortcut for testing ----
-# metaDT <- data.table::fread(paste0(lDataLoc, "metaDT.csv"))
-
 
 figCaption <- paste0("EA Wholesale Generation data ", min(metaDT$dateFrom), " - ", max(metaDT$dateTo),
-                     "\nData: ", lDataLoc)
+                     "\nData: ", oDataPath)
 
 makeCheckPlot <- function(dt,fillVar){
   # makes a tile plot of month by year and fills colour by the indicator chosen
@@ -137,7 +138,7 @@ makeCheckPlot <- function(dt,fillVar){
          title = paste0("Data check chart:", eval(fillVar))
          )
 
-  ggplot2::ggsave(lDataLoc,paste0(eval(fillVar),"Plot.pdf"))
+  ggplot2::ggsave(oDataPath,paste0(eval(fillVar),"Plot.pdf"))
 
   return(myPlot)
 }
@@ -148,7 +149,7 @@ makeCheckPlot(metaDT, "sumkWh")
 makeCheckPlot(metaDT, "nFuels")
 
 # write out the meta data ----
-data.table::fwrite(metaDT, paste0(lDataLoc, "metaDT.csv"))
+data.table::fwrite(metaDT, paste0(oDataPath, "metaDT.csv"))
 
 # remove the temp file
 file.remove("temp.csv")
