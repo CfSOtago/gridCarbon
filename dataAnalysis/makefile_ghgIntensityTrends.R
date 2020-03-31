53edec7b514d2705542f93aa2794d22fc19f8bdc
Ben Anderson <dataknut@gridcrawler.otago.ac.nz>
2020-04-01 12:46:33 +1300

    amended makefile to properly clean data

--- dataAnalysis/makefile_ghgIntensityTrends.R
+++ dataAnalysis/makefile_ghgIntensityTrends.R
@@ -16,6 +16,10 @@ loadLibraries(libs) # should install any that are missing
 # Parameters ----
 localParams <- list()
 
+# > dates
+localParams$lockDownStart <- as.Date("2020-03-24")
+localParams$lockDownEnd <- as.Date("2020-04-24")
+
 # > data paths
 localParams$gridDataLoc <- paste0(gcParams$GreenGrid, 
                                   "externalData/EA_Generation_Data/processed/yearly/")
@@ -59,25 +63,40 @@ origGridDT <- drake::readd(gridData)
 origNonGridDT <- drake::readd(nonGridData)
 
 # fix vars ----
-  # fix vars here so we don't get forced to reload the data when we add new fixes
-  # > grid data ----
+# fix vars here so we don't get forced to reload the data when we add new fixes
+
+# > grid data ----
 origGridDT[, rTime := hms::as_hms(rTime)]
 origGridDT[, rDateTimeOrig := rDateTime] # just in case
 origGridDT[, rDateTime := lubridate::as_datetime(rDateTime)]
 origGridDT[, rDateTime := lubridate::force_tz(rDateTime, tzone = "Pacific/Auckland")]
 origGridDT[, rMonth := lubridate::month(rDateTime, label = TRUE, abbr = TRUE)]
 
+# check
 h <- head(origGridDT[, .(rDateTimeOrig, Time_Period, rDateTime)])
 h
 print("Grid gen loaded")
 message("Loaded ", tidyNum(nrow(origGridDT)), " rows of data")
+table(origGridDT[is.na(rDateTime)]$Time_Period)
+allGridDT <- origGridDT[!is.na(rDateTime)] # removes TP 49 & 50
+nrow(allGridDT)
 
 # > non grid data ----
+origNonGridDT[, rTime := hms::as_hms(rTime)]
+origNonGridDT[, rDateTimeOrig := rDateTime] # just in case
+origNonGridDT[, rDateTime := lubridate::as_datetime(rDateTime)]
+origNonGridDT[, rDateTime := lubridate::force_tz(rDateTime, tzone = "Pacific/Auckland")]
+origNonGridDT[, rMonth := lubridate::month(rDateTime, label = TRUE, abbr = TRUE)]
+# check
 h <- head(origNonGridDT[, .(rDateTimeOrig, Time_Period, rDateTime)])
 h
 print("Non grid gen loaded")
 message("Loaded ", tidyNum(nrow(origNonGridDT)), " rows of data")
-  
+table(origNonGridDT[is.na(rDateTime)]$Time_Period)
+allEmbeddedDT <- origNonGridDT[!is.na(rDateTime)] # removes TP 49 & 50
+nrow(allEmbeddedDT)
+
+
 # GXP data ----
 getGXPFileList <- function(dPath){
   # check for EA GXP files
@@ -88,77 +107,6 @@ getGXPFileList <- function(dPath){
   message("Found ", nrow(dt))
   return(dt)
 } # should be in package functions
-
-
-loadGXPData <- function(files){
-  # load the EA GXP data
-  # https://stackoverflow.com/questions/21156271/fast-reading-and-combining-several-files-using-data-table-with-fread
-  # this is where we need drake
-  # and probably more memory
-  message("Loading ", nrow(files), " GXP files")
-  l <- lapply(files$fullPath, data.table::fread)
-  dt <- rbindlist(l)
-  setkey(dt, rDateTime)
-  try(file.remove("temp.csv")) # side effect
-  # fix dates & times ----
-  dt <- dt[!is.na(rTime)] # drop the TP49 & TP50
-  dt[, rDateTime := lubridate::as_datetime(rDateTime)]
-  dt[, rDateTime := lubridate::force_tz(rDateTime, tzone = "Pacific/Auckland")]
-  dt[, date := lubridate::date(rDateTime)]
-  dt[, month := lubridate::month(rDateTime)]
-  dt[, day_of_week := lubridate::wday(rDateTime, label = TRUE)]
-  dt[, hms := hms::as.hms(rDateTime)] # set to middle of half-hour
-  dt[, halfHour := hms::trunc_hms(hms, 30*60)] # truncate to previous half-hour
-  
-  # Create factor for weekdays/weekends ----
-  dt[, weekdays := "Weekdays"]
-  dt[, weekdays := ifelse(day_of_week == "Sat" |
-                            day_of_week == "Sun", "Weekends", weekdays)]
-  # locate in peak/not peak ----
-  dt <- setPeakPeriod(dt)
-  
-  # load the POC lookup table Vince sent
-  f <- paste0(here::here(), "/data/input/gxp-lookup.csv")
-  gxpLutDT <- data.table::fread(f)
-  
-  setkey(gxpLutDT, node)
-  setkey(dt, POC)
-  
-  dt <- gxpLutDT[dt] # merge them - this keeps all the gxpData
-  
-  # load the Transpower GIS table
-  f <- paste0(here::here(), "/data/input/gxpGeolookup.csv")
-  gxpGeoDT <- data.table::fread(f)
-  # note there are differences
-  # in the Transpower data MXLOCATION == node but without the trailing 0331 - whatever this means
-  dt[, MXLOCATION := substr(node, start = 1, stop = 3)] # note this produces non-unique locations
-  # so presumably some gxps share a location where they feed different networks
-  uniqueN(dt$node)
-  uniqueN(dt$MXLOCATION)
-  dt[, .(nNodes = uniqueN(node)), keyby = .(MXLOCATION)]
-  setkey(dt, MXLOCATION)
-  setkey(gxpGeoDT, MXLOCATION)
-  gxpDataDT <- gxpGeoDT[dt]
-  return(dt)
-} # should be in package functions
-
-years <- c("2015")
-
-gxpFiles <- getGXPFileList(repoParams$gxpData) # will be empty if never run before so
-
-if(nrow(gxpFiles) == 0){
-  message("No data!")
-} else {
-  message("Yep, we've got (some) data, loading it")
-  gxpDataDT <- loadGXPData(gxpFiles)
-  # save out fpr future use
-  message("Saving processed version for future use...")
-  data.table::fwrite(file = paste0(repoParams$gxpData, "gxpDataDT_latest.csv"),
-                     gxpDataDT)
-}
-
-# remove the date NAs here (DST breaks)
-gxpDataDT <- gxpDataDT[!is.na(date)]
 
 # > defn of peak ----
 amPeakStart <- hms::as_hms("07:00:00")
@@ -191,7 +139,7 @@ makeReport <- function(f){
                     params = list(title = title,
                                   subtitle = subtitle,
                                   authors = authors),
-                    output_file = paste0(repoParams$repoLoc,"/docs/", title,"_",
+                    output_file = paste0(gcParams$repoLoc,"/docs/", title,"_",
                                          subtitle,".html")
   )
 }
@@ -207,7 +155,7 @@ authors <- "Ben Anderson, Marilette Lotte and Carsten Dortans"
 
 
 # >> run report ----
-rmdFile <- paste0(gcParams$repoLoc, "/analysis/ghgIntensityTrends.Rmd")
-#makeReport(rmdFile)
+rmdFile <- paste0(gcParams$repoLoc, "/dataAnalysis/ghgIntensityTrends.Rmd")
+makeReport(rmdFile)
 
 
