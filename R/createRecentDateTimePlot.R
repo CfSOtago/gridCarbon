0866869984830f696fdd62238070cc86e2c76a9b
B.Anderson <ba1e12@srv02405.soton.ac.uk>
2020-06-13 17:35:39 +0100

    switched to loess for smoothing

--- R/createRecentDateTimePlot.R
+++ R/createRecentDateTimePlot.R
@@ -38,11 +38,11 @@ createRecentDateTimePlot <- function(dt, dateTime, yVar, yCap, yDiv = 1, lockDow
     scale_x_datetime(date_breaks = "7 day", date_labels =  "%a %d %b")  +
     theme(axis.text.x=element_text(angle=90, hjust=1)) +
     labs(caption = paste0(localParams$lockdownCap, localParams$weekendCap,
-                          "\n", localParams$gamCap),
+                          "\n", localParams$loessCap),
          x = "Time",
          y = yCap) +
     theme(legend.position = "bottom") + 
-    geom_smooth(aes(colour = NULL)) +
+    geom_smooth(aes(colour = NULL), method = "loess") +
     scale_color_viridis_d(name = "Day of the week") +
     guides(colour=guide_legend(nrow=2))
   
