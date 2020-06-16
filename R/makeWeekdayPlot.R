76260d6caebccd9d541153e891915206d764833e
B.Anderson <ba1e12@srv02405.soton.ac.uk>
2020-06-16 17:28:05 +0100

    added 2 plot functions to mimic openair's timeVariation

---
+++ R/makeWeekdayPlot.R
@@ -0,0 +1,32 @@
+#' Creates a plot by weekday from the data.table given
+#'
+#' `makeWeekdayPlot` returns a plot which shows yVar by weekday, compareYear and plotPeriod (assumed to be lockdown). 
+#' Assumes this is the date aligned data.
+#'
+#' @param dt the data
+#' @param yVar the variable you want to plot
+#' @param yLab the label for the y axis
+#' @param yDiv the value you want to divide yVar by to make the y axis more sensible. Default = `1`
+#' 
+#' @return a plot
+#' @import ggplot2
+#' @import data.table
+#' @author Ben Anderson, \email{b.anderson@@soton.ac.uk}
+#' @export
+#' @family plot
+#'
+makeWeekdayPlot <- function(dt, yVar, yLab, yDiv){
+  # by weekday 
+  # not proportions - absolute
+  # wkdayFixed = obs (they are the same - that was the whole idea!)
+  pDT <- plotDT[, .(yVals = mean(get(yVar))/yDiv), keyby = .(plotPeriod, compareYear, wkdayFixed)]
+  p <- ggplot2::ggplot(pDT, aes(x = wkdayFixed, y = yVals, 
+                               colour = compareYear, group = compareYear)) + 
+    geom_line() +
+    theme(legend.position="bottom") +
+    scale_color_discrete(name="Year") +
+    facet_grid(plotPeriod ~ . ) +
+    labs( y = yLab,
+          x = "Weekday")
+  return(p)
+}
