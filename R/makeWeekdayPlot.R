1f1b30f9bc1f500b59e8b6846064015fb3918482
B.Anderson <ba1e12@srv02405.soton.ac.uk>
2020-06-17 13:32:50 +0100

    updated function so we can specify the xVar (week day)

--- R/makeWeekdayPlot.R
+++ R/makeWeekdayPlot.R
@@ -1,9 +1,10 @@
 #' Creates a plot by weekday from the data.table given
 #'
-#' `makeWeekdayPlot` returns a plot which shows yVar by weekday, compareYear and plotPeriod (assumed to be lockdown). 
-#' Assumes this is the date aligned data.
+#' `makeWeekdayPlot` returns a box plot which showing yVar by weekday, compareYear and plotPeriod (assumed to be lockdown). 
+#' 
 #'
 #' @param dt the data
+#' @param xVar the weekday variable you want to use
 #' @param yVar the variable you want to plot
 #' @param yLab the label for the y axis
 #' @param yDiv the value you want to divide yVar by to make the y axis more sensible. Default = `1`
@@ -15,14 +16,15 @@
 #' @export
 #' @family plot
 #'
-makeWeekdayPlot <- function(dt, yVar, yLab, yDiv){
+makeWeekdayPlot <- function(dt, xVar, yVar, yLab, yDiv){
   # by weekday 
   # not proportions - absolute
   # wkdayFixed = obs (they are the same - that was the whole idea!)
-  pDT <- dt[, .(yVals = mean(get(yVar))/yDiv), keyby = .(plotPeriod, compareYear, wkdayFixed)]
-  p <- ggplot2::ggplot(pDT, aes(x = wkdayFixed, y = yVals, 
-                               colour = compareYear, group = compareYear)) + 
-    geom_line() +
+  dt[, xVar := get(xVar)]
+  pDT <- dt[, .(yVals = mean(get(yVar))/yDiv), keyby = .(plotPeriod, compareYear, xVar)]
+  p <- ggplot2::ggplot(dt, aes(x = xVar, y = GWh, 
+                               colour = compareYear)) + 
+    geom_boxplot() +
     theme(legend.position="bottom") +
     scale_color_discrete(name="Year") +
     facet_grid(plotPeriod ~ . ) +
