7513d572c87381638454208e36496219d329ad8d
Ben Anderson <dataknut@icloud.com>
2020-07-09 23:42:25 +0100

    added tests that should run

---
+++ tests.R
@@ -0,0 +1,67 @@
+library(gridCarbon)
+library(ggplot2)
+library(data.table)
+
+source("env.R")
+repoParams$ukGridDataLoc
+
+localParams <- list()
+localParams$fromYear <- 2019
+localParams$toDate <- as.Date("2020-06-30")
+
+# should mostly be in R/
+makeOutcomes <- function(dt){
+  dt[, rGENERATION := GENERATION * 0.92] # 8% losses (Staffel, 2017)
+  dt[, consumption_MW := rGENERATION]
+  dt[, consGW := consumption_MW/1000]
+  dt[, consumption_MWh := consumption_MW/2] # total MWh per half hour = power / 2
+  dt[, consGWh := consumption_MWh/1000]
+  
+  # Total CO2e - original grid gen data
+  # CI = g CO2e/kWh
+  dt[, C02e_g := (1000*consumption_MWh) * CARBON_INTENSITY]
+  dt[, C02e_kg := C02e_g/1000]
+  dt[, C02e_T := C02e_kg/1000 ]
+  return(dt)
+}
+
+gridGenData <- gridCarbon::loadUKESOYearlyGenData(path = paste0(repoParams$ukGridDataLoc, "processed/yearly/"), # from where?
+                                     fromYear = localParams$fromYear, # from what date?
+                                     toDate = localParams$toDate # to when?
+                                     )
+derivedGridGenData <- makeOutcomes(gridGenData)
+alignedGridGenData <- gridCarbon::alignDates(derivedGridGenData, 
+                                dateTime = "rDateTimeUTC",
+                                toDate = localParams$toDate)
+
+recentDateTimeGWhPlot <- createRecentDateTimePlot(derivedGridGenData,
+                                  dateTime = "rDateTimeUTC", 
+                                  yVar = "consGWh",
+                                  yCap = "GWh",
+                                  yDiv = 1,
+                                  lockDownStart = repoParams$UKlockDownStartDateTime,
+                                  lockDownEnd = repoParams$UKlockDownEndDateTime)
+
+recentHalfHourlyProfileGWhPlot <- createRecentHalfHourlyProfilePlot(derivedGridGenData,
+                                                                    dateTime = "rDateTimeUTC",
+                                                                    yVar = "consGWh",
+                                                                    yCap = "GWh",
+                                                                    yDiv = 1)
+
+compareDailyGWhPlot <- createDailyMeanComparePlot(alignedGridGenData,
+                                                 yVar = "consGWh",
+                                                 yCap = "GWh",
+                                                 yDiv = 1,
+                                                 form = "step", # default
+                                                 lockDownStart = repoParams$UKlockDownStartDate,
+                                                 lockDownEnd = repoParams$UKlockDownEndDate
+)
+
+compareDailyGWhpcPlot <- createDailyPcComparePlot(alignedGridGenData,
+                                                 yVar = "consGWh",
+                                                 yCap = "% difference",
+                                                 lockDownStart = repoParams$UKlockDownStartDate,
+                                                 lockDownEnd = repoParams$UKlockDownEndDate
+)
+
+
